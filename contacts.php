61d093786552e8a0236f5ea906443a504351aa43
Hamza Ahmad <hamza.ahmad0932@gmail.com>
2019-11-29 23:16:25 +0500

    old theme

---
+++ contacts.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="en">
+     <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+     <title>Ideal Financial Consultancy</title>
+     
+	  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+     <link rel="icon" href="images/favicon.ico">
+     <link rel="shortcut icon" href="images/favicon.ico" />
+     <link rel="stylesheet" href="css/style.css">
+	 <link rel="stylesheet" href="css/zerogrid.css" type="text/css" media="screen">
+	<link rel="stylesheet" href="css/responsive.css" type="text/css" media="screen"> 
+     <link rel="stylesheet" href="css/form.css">
+     <script src="js/jquery.js"></script>
+     <script src="js/jquery-migrate-1.1.1.js"></script>
+     <script src="js/superfish.js"></script>
+     <script src="js/jquery.easing.1.3.js"></script>
+     <script src="js/sForm.js"></script>
+     <script src="js/Forms.js"></script>
+	 <script src="js/css3-mediaqueries.js"></script>
+     <!--[if lt IE 8]>
+       <div style=' clear: both; text-align:center; position: relative;'>
+         <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+           <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
+         </a>
+      </div>
+    <![endif]-->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+      <link rel="stylesheet" media="screen" href="css/ie.css">
+    <![endif]-->
+     </head>
+     <body>
+       <div class="main">
+<!--==============================header=================================-->
+ <header> 
+  <div class="zerogrid">
+    <div class="col-full">
+	<div class="wrap-col">
+    <h1><a href="index.html"><img src="images/logo.png" alt="EXTERIOR"></a> </h1>
+    
+         <div class="menu_block">
+           <nav>
+            <ul class="sf-menu">
+                   <li><a href="index.html">Home</a></li>
+                   <li><a href="about.html">About</a></li>
+				   	
+				   
+                   <li><a href="services.html">Services</a></li>
+                   <li><a href="career.html">Career</a></li>
+                   <li class="current"><a href="contacts.php">Contact</a></li>
+                 </ul>
+           </nav>
+           <div class="clear"></div>
+           </div>
+           <div class="clear"></div>
+		</div>
+      </div>
+    </div>
+</header>
+<!--=======content================================-->
+
+<div class="content">
+  <div class="zerogrid">
+  <h2>Ideal Financial Consultancy Limited is a limited company, registered under Companies Act, 2006 in Engalnd and Wales, <b>Company Number is 09075725</b>.</h2>
+
+  	<div class="row">
+    <div class="col-2-5">
+		<div class="wrap-col">
+      <h2>Find Us</h2>
+            <div class="map">
+            <figure class="img_inner">
+                          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d18997.97345765003!2d-2.2158765!3d53.4729872!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487bb1399134829f%3A0xf38f77d5adfeec0f!2sIdeal%20financial%20consultancy%20limited!5e0!3m2!1sen!2suk!4v1573511085789!5m2!1sen!2suk" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+               </figure>
+              <address>
+              <h2>Directors:</h2>
+                            <dl>
+                  <dt><p><b><a href="#">Muhammad Arslan</a></b>  <br>
+                   <b> Mobile:</b> &nbsp;&nbsp;0741 206 0708<br>
+                 <br> <b><a href="#">Rehman S K Abdali</a></b><br>
+                 <b> Mobile</b>:&nbsp;&nbsp; 0742 8791 609 </p>
+                                </dt>
+                                              <h2>Mailing Address:</h2>
+
+                                <dd><span>24 Midland Street</span></dd>
+                                <dd><span>Manchester</span></dd>
+                                <dd><span>M12 6LB</span></dd>
+                            </dl>
+                                
+                                
+                            </dl>
+                         </address>
+ 
+          </div>
+	</div>
+    </div>
+	<div class="col-1-5"><div class="wrap-col"><!--empty--></div></div>
+    <div class="col-2-5">
+	<div class="wrap-col">
+      <h2>Contact Us</h2>
+      <form id="form">
+      <div class="success_wrapper">
+      <div class="success">Contact form submitted!<br>
+      <strong>We will be in touch soon.</strong> </div></div>
+      <fieldset>
+      <label class="name">
+      <input type="text" value="Name:">
+      <br class="clear">
+      <span class="error error-empty">*This is not a valid name.</span><span class="empty error-empty">*This field is required.</span> </label>
+      <label class="email">
+      <input type="text" value="E-mail:">
+      <br class="clear">
+      <span class="error error-empty">*This is not a valid email address.</span><span class="empty error-empty">*This field is required.</span> </label>
+      <label class="phone">
+      <input type="tel" value="Phone:">
+      <br class="clear">
+      <span class="error error-empty">*This is not a valid phone number.</span><span class="empty error-empty">*This field is required.</span> </label>
+      <label class="message">
+      <textarea>Message:</textarea>
+      <br class="clear">
+      <span class="error">*The message is too short.</span> <span class="empty">*This field is required.</span> </label>
+      <div class="clear"></div>
+      <div class="btns"><a data-type="reset" class="btn">clear</a><a data-type="submit" class="btn">send</a>
+      <div class="clear"></div>
+      </div></fieldset></form>
+	 </div>
+   <br> <br> <h2>Contact Details:</h2>
+                                <dd><span><b>Tel:</b></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;0161 349 2349</dd><br>
+                                <dd><span><b>Email:</b></span>&nbsp;&nbsp;&nbsp;&nbsp;<a href="#">info@Idealconsultant.co.uk</a></dd>
+    </div>
+    
+    </div>
+    
+	
+    <div class="row">
+    <div class="bottom_block">
+      <div class="col-1-2">
+        <h3>Follow Us</h3>
+        <div class="socials">
+          <a href="https://www.facebook.com/pages/IDEAL-Financial-Consultancy/246126778916765?ref=hl" target="_blank" rel="nofollow"></a>
+        </div>
+        <nav><ul>
+                   <li><a href="index.html">Home</a></li>
+                   <li><a href="about.html">About</a></li>
+				   	
+				   
+                   <li><a href="services.html">Services</a></li>
+                   <li><a href="career.html">Career</a></li>
+<li class="current"><a href="contacts.php">Contact</a></li>
+                 </ul></nav>
+      </div>
+      <div class="col-1-2">
+        <h2>Office Timings:</h2>
+
+        <dd><span>Monday to Friday:&nbsp;&nbsp;&nbsp;&nbsp; 9am to 5pm</span></dd>
+    <dd><span>Saturday:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Closed</span></dd>
+         <dd><span>Sunday:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Closed</span></dd>
+          </div>
+      </div>
+	  </div>
+    </div>
+  </div>
+</div>
+<!--==============================footer=================================-->
+
+<footer>    
+  <div class="zerogrid">
+    <div class="col-full">
+		
+    </div>
+  </div>
+</footer>
+</body>
+</html>
